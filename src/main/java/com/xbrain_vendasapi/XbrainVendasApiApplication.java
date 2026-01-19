27dbf6835347ab2aec9247950e5213ec05c7635e
Carlos Ramos <caarloos1997@hotmail.com>
2026-01-19 20:56:54 -0300

    config inicial do projeto vendas com Spring Boot

---
+++ src/main/java/com/xbrain_vendasapi/XbrainVendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain_vendasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainVendasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xbrain_vendasapi/XbrainVendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain_vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainVendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
